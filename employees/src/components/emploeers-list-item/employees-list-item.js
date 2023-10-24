3bcd4e45d4d5ded3505524fc5bc0519859069a5c
S-E-Pashin <59841297+S-E-Pashin@users.noreply.github.com>
2023-10-24 14:14:47 +0300

    Constructor class + state
      // onIncrease = () => {
      //   this.setState((state) => ({
      //     increase: !state.increase
      //   }))
      // }
    
      // Оба варианта верны. В варианте ниже передаваемый потов(В будущем state) сразу деструктуризируется и в него передается значение из increase и дальше в значение increase записывается противоположное ему булево значение.
    
      onIncrease = () => {
        this.setState(({increase}) => ({
          increase: !increase
        }))
      }

--- employees/src/components/emploeers-list-item/employees-list-item.js
+++ employees/src/components/emploeers-list-item/employees-list-item.js
@@ -4,9 +4,30 @@ import {Component} from "react/index";
 import './employees-list-item.css';
 
 class EmployeesListItem extends Component {
-  render() {
-  const {name, salary, increase} = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+    }
+  }
 
+  // onIncrease = () => {
+  //   this.setState((state) => ({
+  //     increase: !state.increase
+  //   }))
+  // }
+
+  // Оба варианта верны. В варианте ниже передаваемый потов(В будущем state) сразу деструктуризируется и в него передается значение из increase и дальше в значение increase записывается противоположное ему булево значение.
+
+  onIncrease = () => {
+    this.setState(({increase}) => ({
+      increase: !increase
+    }))
+  }
+
+  render() {
+  const {name, salary} = this.props;
+  const {increase} = this.state;
     let classNames = 'list-group-item d-flex justify-content-between';
 
     if (increase) {
@@ -20,7 +41,8 @@ class EmployeesListItem extends Component {
 
         <div className='d-flex justify-content-center align-items-center'>
           <button type="button"
-                  className="btn-cookie btn-sm ">
+                  className="btn-cookie btn-sm "
+          onClick={this.onIncrease}>
             <i className="fas fa-cookie"></i>
           </button>
 
